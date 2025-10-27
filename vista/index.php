3b4794bd882f9ef1a5e7bdd20e80bc967b3154e5
IsaiasEchegaray <saiasiech@gmail.com>
2025-10-27 17:27:54 -0600

    test de conexion por si lo quieren

--- vista/index.php
+++ vista/index.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-<html lang="es">
+<?php
+    include_once "../modelos/conection/conection.php";
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
